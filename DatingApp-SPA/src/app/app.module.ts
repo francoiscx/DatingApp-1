5db33040d49d3f0bf43f70ed4bdf50de830b315c
Ruan Davis <ruandavis258@gmail.com>
2020-03-27 10:56:18 +0200

    end of section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      RegisterComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
@@ -20,7 +22,7 @@ import { AuthService } from './_services/auth.service';
       FormsModule
    ],
    providers: [
-     AuthService
+      AuthService
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,28 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe( next => {
+      console.log('logged in successfully.');
+    }, error => {
+      console.log('Loggin unsuccessfull.');
+    });
   }
+  // get token  the double ! is shorthand for true and false
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return!!token;
+}
+// Remove authorising token from storrage therefroe logging out securely
+logOut(){
+  localStorage.removeItem('token');
+  console.log('You have successfully logged out!');
+}
+
 }
