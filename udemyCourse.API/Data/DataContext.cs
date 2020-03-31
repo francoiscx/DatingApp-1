5af2a9df4a568deeff00fc817c8024a7b3ff880c
Ruan Davis <ruandavis258@gmail.com>
2020-03-31 16:24:46 +0200

    update

--- udemyCourse.API/Data/DataContext.cs
+++ udemyCourse.API/Data/DataContext.cs
@@ -8,8 +8,9 @@ namespace udemyCourse.API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
-
+// setting databse with tables
         public DbSet<value> Values { get; set; }
        public DbSet<User> Users { get; set; }
+       public DbSet <Photo> photos { get; set; }
     }
 }
